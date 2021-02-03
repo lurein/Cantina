ad5ba90b0b937d0acfa2fbe63bc9aa6dbb66c030
Lurein Perera <pererag@bc.edu>
2021-02-03 02:03:11 -0500

    first commit

---
+++ src/main/index.js
@@ -0,0 +1,73 @@
+'use strict'
+
+import { app, BrowserWindow, BrowserView } from 'electron'
+
+/**
+ * Set `__static` path to static files in production
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ */
+if (process.env.NODE_ENV !== 'development') {
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+}
+
+let mainWindow
+const winURL = process.env.NODE_ENV === 'development'
+  ? `http://localhost:9080`
+  : `file://${__dirname}/index.html`
+
+function createWindow () {
+  /**
+   * Initial window options
+   */
+  mainWindow = new BrowserWindow({
+    height: 600,
+    useContentSize: true,
+    width: 1000
+  })
+  let browserView = new BrowserView({ webPreferences: { nodeIntegration: false } })
+
+  mainWindow.loadURL(winURL)
+  mainWindow.setBrowserView(browserView)
+
+  browserView.setBounds({ x: 160, y: 0, width: 840, height: 600 })
+  browserView.setAutoResize({ width: true, height: true, horizontal: true, vertical: true })
+  browserView.webContents.loadURL('https://www.givecard.io/')
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
+
+/**
+ * Auto Updater
+ *
+ * Uncomment the following code below and install `electron-updater` to
+ * support auto updating. Code Signing with a valid certificate is required.
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
+ */
+
+/*
+import { autoUpdater } from 'electron-updater'
+
+autoUpdater.on('update-downloaded', () => {
+  autoUpdater.quitAndInstall()
+})
+
+app.on('ready', () => {
+  if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
+})
+ */
---
+++ src/renderer/components/Dock.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="sidebar">
+    <a href="#">About</a>
+    <a href="#">About</a>
+    <a href="#">About</a>
+    <a href="#">About</a>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.sidebar {
+  height: 100%;
+  width: 160px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
+
+.sidebar a {
+  padding: 6px 8px 6px 16px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+}
+</style>
